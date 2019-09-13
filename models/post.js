4833b93c4b2371f081b4a0bf7c62eafb797400a4
claredowson <clare.dowson@barnardos.org.uk>
2019-09-13 15:23:17 +0100

    date added

--- models/post.js
+++ models/post.js
@@ -2,6 +2,10 @@ var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
   message: String,
+  created: {
+       type: Date,
+       default: Date.now
+   }
 });
 
 var Post = mongoose.model('Post', PostSchema);
