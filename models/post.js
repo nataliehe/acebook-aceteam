97224e367761d97fd9eaeef14a83aa3571cffc76
assadk88 <assadkhan@outlook.com>
2019-09-13 15:18:55 +0100

    Add created property to Post schema

--- models/post.js
+++ models/post.js
@@ -2,8 +2,14 @@ var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
   message: String,
+  created: {
+    type: String,
+    default: "Hello, world!"
+  }
 });
 
 var Post = mongoose.model('Post', PostSchema);
 
 module.exports = Post;
+
+
