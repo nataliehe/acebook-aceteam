e96508c3542ee2a0b489beb4af5312e2239b02b0
assadk88 <assadkhan@outlook.com>
2019-09-23 17:33:32 +0100

    Add comments to formatted posts obj

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,12 +2,15 @@ var Post = require('../models/post');
 
 var PostsController = {
   Index: function(req, res) {
+
     Post.find({}).sort({created: -1}).exec(function(err, posts) {
       if (err) { throw err; }
       var posts_formatted = posts.map( post => {
         var post_f = {};
         post_f["message"] = post.message;
         post_f["created"] = post.created.toString().slice(0,21);
+        post_f["id"] = post._id;
+        post_f["comments"] = post.comments;
         return post_f;
       });
       res.render('posts/index', { posts: posts_formatted });
@@ -15,7 +18,7 @@ var PostsController = {
   },
 
   Create: function(req, res) {
-    var post = new Post({ message: req.body.message, created: req.body.created });
+    var post = new Post({ message: req.body.message });
     post.save(function(err) {
       if (err) { throw err; }
 
